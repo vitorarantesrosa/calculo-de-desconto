d0c3f00d388c3b439f2e754e65bbf9ec77a229ce
Vitor Rosa <319534144+vitorarantesrosa@users.noreply.github.com>
2026-09-11 15:15:50 -0300

    Add files via upload

---
+++ VitorArantes_Ag6_DS_I.py
@@ -0,0 +1,35 @@
+# input inicial
+compra = float(input("Qual o valor da compra em R$? "))
+
+# variável para cálculo do desconto
+desconto = 0
+
+# separador
+print("============================================")
+
+# identação
+if compra < 200:
+    desconto = 0.05
+    print ("Você ganhou um desconto de 5%!")
+elif compra >= 200 and compra <= 300:
+    desconto = 0.10
+    print ("Você ganhou um desconto de 10%!")
+elif compra > 300:
+    print ("Você ganhou um desconto de 15%!")
+    desconto = 0.15
+
+# separador
+print("============================================")
+
+# processamento
+calculo = compra * desconto
+valorfinal = compra - calculo
+
+# mensagem final
+print(f"O valor a pagar é RS${valorfinal:.2f}")
+
+# separador
+print("============================================")
+
+# mensagem para finalizar o programa e deixar os resultados anteriores à mostra
+input("Digite ENTER para encerrar o programa.")
